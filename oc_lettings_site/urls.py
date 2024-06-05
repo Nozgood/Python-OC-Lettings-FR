72ae754ca02a13b29f7f1f0102e96c8f5116b568
Nowfeel Safi <nowfeel@MacBook-Air-de-Nowfeel.local>
2024-06-05 07:57:19 +0200

    refactor: single app profiles (moved from oc-lettings app) deleted old tables of oc-lettings and create dir for templates

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -4,9 +4,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('lettings/', include("lettings.urls", namespace="lettings")),
-    path('profiles/', views.profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', views.profile, name='profile'),
-    path('admin/', admin.site.urls),
+    path("", views.index, name="index"),
+    path("lettings/", include("lettings.urls", namespace="lettings")),
+    path("profiles/", include("profiles.urls", namespace="profiles")),
+    path("admin/", admin.site.urls),
 ]
